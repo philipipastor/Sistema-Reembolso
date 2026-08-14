0282e5efa8b96ab8a60a1b8880752ca5bc9fc966
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 21:12:53 -0300

    chore: configurar ponto de entrada da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { BrowserRouter } from 'react-router'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </StrictMode>,
 )
