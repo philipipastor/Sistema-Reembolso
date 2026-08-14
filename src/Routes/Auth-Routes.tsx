e3f7b9c1c9522f424db96fd4227a5d764e4cdb56
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 22:40:24 -0300

    feat: adiciona página 404

--- src/Routes/Auth-Routes.tsx
+++ src/Routes/Auth-Routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router"
 import { AuthLayout } from "../components/AuthLayout"
 import { Login } from "../pages/Login"
 import { Cadastro } from "../pages/Cadastro"
+import { NotFound } from "../pages/NotFound"
 
 export function AuthRoutes(){
     return(
@@ -11,6 +12,8 @@ export function AuthRoutes(){
                 <Route path="/" element={<Login/>}/>
                 <Route path="/cadastro" element={<Cadastro/>}/>
             </Route>
+
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
     )
 }
