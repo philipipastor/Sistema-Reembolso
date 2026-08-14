30e3da9dc89e44fce911c1747ee6214a2de68edf
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 22:22:45 -0300

    feat: adicionar rota de cadastro

--- src/Routes/Auth-Routes.tsx
+++ src/Routes/Auth-Routes.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router"
 
-import { Login } from "../pages/Login"
 import { AuthLayout } from "../components/AuthLayout"
+import { Login } from "../pages/Login"
+import { Cadastro } from "../pages/Cadastro"
 
 export function AuthRoutes(){
     return(
         <Routes>
             <Route element={<AuthLayout/>}>
-                <Route path="/login" element={<Login/>}/>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/cadastro" element={<Cadastro/>}/>
             </Route>
         </Routes>
     )
