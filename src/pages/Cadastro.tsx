91e5eef637404393729b61b5091037a2a767e253
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 22:46:58 -0300

    feat: implementa link para voltar à tela de login

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -1,15 +1,20 @@
 import { Input } from "../components/input.tsx"
 import { Button } from "../components/Button.tsx"
+import { Link } from "../components/Link.tsx"
 
 export function Cadastro() {
     return(
-        <form className="w-full flex flex-col gap-4">
-            <Input legenda="Nome"/>
-            <Input legenda="e-mail"/>
-            <Input legenda="senha" type="passsword"/>
-            <Input legenda="confirme a senha" type="passsword"/>
-            <Button>Cadastrar</Button>
-        </form>
+        <div>
+            <form className="w-full flex flex-col gap-4">
+                <Input legenda="Nome"/>
+                <Input legenda="e-mail"/>
+                <Input legenda="senha" type="passsword"/>
+                <Input legenda="confirme a senha" type="passsword"/>
+                <Button>Cadastrar</Button>
+            </form>
+
+            <Link href="/">Já tenho uma conta</Link>
+        </div>
 
 
     )
