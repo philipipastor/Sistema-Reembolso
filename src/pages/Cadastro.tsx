844ffd79cefb0d15bd3802263c7bfac41122c7ce
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 23:05:03 -0300

    feat: adiciona formulário de cadastro e link de login

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -6,14 +6,16 @@ export function Cadastro() {
     return(
         <div>
             <form className="w-full flex flex-col gap-4">
-                <Input legenda="Nome"/>
-                <Input legenda="e-mail"/>
-                <Input legenda="senha" type="passsword"/>
-                <Input legenda="confirme a senha" type="passsword"/>
+                <Input legenda="Nome" placeholder="Informe seu nome"/>
+                <Input legenda="e-mail" placeholder="Informe seu e-mail"/>
+                <Input legenda="senha" type="passsword" placeholder="Informe sua senha"/>
+                <Input legenda="confirme a senha" type="passsword" placeholder="Confirme sua senha"/>
+
                 <Button>Cadastrar</Button>
+
+                <Link href="/">Já tenho uma conta</Link>
             </form>
 
-            <Link href="/">Já tenho uma conta</Link>
         </div>
 
 
