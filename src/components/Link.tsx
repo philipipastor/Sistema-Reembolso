afa5370a3ec8247c12944a08f6eab1c7fddf2118
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 23:04:06 -0300

    styles: adiciona estilização

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,6 +2,6 @@ type Props = React.ComponentProps<"a">
     
 export function Link({children, ...rest}: Props){
     return(
-        <a {...rest}>{children}</a>
+        <a className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"{...rest}>{children}</a>
     )
 }
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,12 @@
+import { Link } from "../components/Link"
+
 export function NotFound(){
     return(
-        <h1>Página não encontrada</h1>
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="flex flex-col">
+                <h1 className="text-gray-100 font-semibold text-2xl mb-10">Página não encontrada</h1>
+                <Link href="/">Voltar para o ínicio</Link>
+            </div>
+        </div>
     )
 }
