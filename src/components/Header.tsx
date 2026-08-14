51a530a0c48ff5cb813cdecc2ca9eba22eff18b7
Philipi Pastor <philipipastor123@gmail.com>
2026-08-14 16:49:28 -0300

    feat: adicionar componente Header reutilizável

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.svg"
+import logout from "../assets/logout.svg"
+
+export function Header(){
+    return(
+        <header className="w-full flex justify-between">
+            <img className="my-8"src={logo} alt="logo" />
+
+            <div className="flex items-center gap-3">
+                <span className="text-sm font-semibold text-gray-200">Olá, Rodrigo</span>
+
+                <img className="my-8 cursor-pointer hover:opacity-75 transition ease-linear" src={logout} alt="ícone de sair"/>
+            </div>
+        </header>
+    )
+}
