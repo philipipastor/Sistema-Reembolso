0efe765e80e3cc32328f25b080f2988e2499f8ee
Philipi Pastor <philipipastor123@gmail.com>
2026-08-13 22:29:36 -0300

    feat: criar componente button reutilizável

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,13 @@
 type Props = React.ComponentProps<"button"> & {
-    legenda: string
+    isLoading?: boolean
 }
 
-export function Button({legenda ,...rest}: Props){
+export function Button({children, isLoading, type="button" ,...rest}: Props){
     return(
-        <button className= "w-sm h-12 bg-green-100 text-amber-50 rounded-lg" {...rest}>{legenda}</button>
+        <button className= "flex items-center justify-center w-sm h-12 bg-green-100 text-white rounded-lg cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed" 
+        type={type} 
+        disabled={isLoading}
+        {...rest}
+        >{children}</button>
     )
 }
