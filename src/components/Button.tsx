aa89fd86ebbdc04575d07ec1f90d5300d3e09903
Philipi Pastor <philipipastor123@gmail.com>
2026-08-14 23:37:10 -0300

    chore: preparar botão para estado de loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,9 +4,9 @@ type Props = React.ComponentProps<"button"> & {
 
 export function Button({children, isLoading, type="button" ,...rest}: Props){
     return(
-        <button className= "flex items-center justify-center w-sm h-12 bg-green-100 text-white rounded-lg cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed" 
+        <button className= "flex items-center justify-center w-full h-12 bg-green-100 text-white rounded-lg cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed" 
         type={type} 
-        disabled={isLoading}
+        disabled={false}
         {...rest}
         >{children}</button>
     )
