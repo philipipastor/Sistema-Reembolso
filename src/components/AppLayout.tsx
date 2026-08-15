b467a15ae8034caa77c356a23de5f82da4a4e9d4
Philipi Pastor <philipipastor123@gmail.com>
2026-08-14 23:36:02 -0300

    refactor: removendo import desnecessário

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,5 +1,3 @@
-import logo from "../assets/logo.svg"
-
 import { Outlet } from "react-router"
 
 import { Header } from "./Header"
@@ -8,7 +6,7 @@ export function AppLayout(){
     return(
         <div className="w-screen h-screen bg-gray-400 flex flex-col items-center text-gray-100">
             <main className="p-3 w-full md:w-auto">
-                <Header/>
+                <Header />
                 <Outlet />
             </main>
         </div>
