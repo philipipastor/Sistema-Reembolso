703bf74100143388d400520e70d6c0d71aa2fc6b
Philipi Pastor <philipipastor123@gmail.com>
2026-08-15 15:12:52 -0300

    refactor: altera posição do componente no AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -4,9 +4,9 @@ import { Header } from "./Header"
 
 export function AppLayout(){
     return(
-        <div className="w-screen h-screen bg-gray-400 flex flex-col items-center text-gray-100">
+        <div className="w-screen h-screen bg-gray-400 flex flex-col items-center text-gray-100 p">
+            <Header />
             <main className="p-3 w-full md:w-auto">
-                <Header />
                 <Outlet />
             </main>
         </div>
