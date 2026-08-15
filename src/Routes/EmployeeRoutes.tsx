ae790a657624c1a8a4c280adf8c4de7e68e214db
Philipi Pastor <philipipastor123@gmail.com>
2026-08-15 15:10:03 -0300

    feat: adicionando rota de confirmação

--- src/Routes/EmployeeRoutes.tsx
+++ src/Routes/EmployeeRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router"
 
 import { AppLayout } from "../components/AppLayout"
 import { Refund } from "../pages/Refund"
+import { Confirm } from "../pages/Confirm"
 import { NotFound } from "../pages/NotFound"
 
 export function EmployeeRoutes(){
@@ -9,6 +10,7 @@ export function EmployeeRoutes(){
         <Routes>
             <Route element={<AppLayout/>}>
                 <Route path="/" element={<Refund/>}/>
+                <Route path="/confirm" element={<Confirm/>}/>
             </Route>
 
             <Route path="*" element={<NotFound/>}/>
