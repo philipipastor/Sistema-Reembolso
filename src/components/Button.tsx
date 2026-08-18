24c360962a6ed483a1e58a42f7a3d4737df6d04c
Philipi Pastor <philipipastor123@gmail.com>
2026-08-17 22:37:32 -0300

    feat: criar variante para componente button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,21 @@
+import { classMerge } from "../utils/ClassMerge"
+
 type Props = React.ComponentProps<"button"> & {
-    isLoading?: boolean
+    isLoading?: boolean,
+    variant?: "default" | "icon" | "iconSmall",
 }
 
-export function Button({children, isLoading, type="button" ,...rest}: Props){
+const variants = {
+    button: {
+        default: "h-12",
+        icon: "h-12 w-12",
+        iconSmall: "h-8 w-8"
+    }
+}
+
+export function Button({children, isLoading, type="button", variant="default" ,...rest}: Props){
     return(
-        <button className= "flex items-center justify-center w-full h-12 bg-green-100 text-white rounded-lg cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed" 
+        <button className= {classMerge(["flex items-center justify-center w-full h-12 bg-green-100 text-white rounded-lg cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-not-allowed", variants.button[variant]])}
         type={type} 
         disabled={false}
         {...rest}
