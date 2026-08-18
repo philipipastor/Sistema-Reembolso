8f1f1e445197b719b5facf4be4c3ff5d05f5a0af
Philipi Pastor <philipipastor123@gmail.com>
2026-08-18 11:19:28 -0300

    chore: organizando frontend e backend

--- src/components/Link.tsx
+++
@@ -1,7 +0,0 @@
-type Props = React.ComponentProps<"a">
-    
-export function Link({children, ...rest}: Props){
-    return(
-        <a className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"{...rest}>{children}</a>
-    )
-}
--- src/utils/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-export function formatCurrency(value: number) {
-    const currency = Intl.NumberFormat("pt-br", {
-        style: "currency",
-        currency: "BRL",
-    })
-
-    return currency.format(value).replace("R$", "")
-}
