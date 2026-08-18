b946abe3ccf769a41a8a72ad42241d5c9bc88c9c
Philipi Pastor <philipipastor123@gmail.com>
2026-08-18 17:50:31 -0300

    styles: adicionando nova variant ao componente Input

--- web/src/components/input.tsx
+++ web/src/components/input.tsx
@@ -2,13 +2,14 @@ import { classMerge } from "../utils/ClassMerge"
 
 type Props = React.ComponentProps<"input"> & {
     legenda?: string,
-    variant?: "default" | "manager"
+    variant?: "default" | "manager" | "inputC"
 }
 
 const variants = {
     input: {
         default: "w-full",
-        manager: "w-232.5"
+        manager: "w-232.5",
+        inputC: "w-96"
     }
 }
 
@@ -16,7 +17,7 @@ export function Input({legenda, type="text", variant="default", ...rest}: Props)
     return(
         <fieldset className="flex flex-1 max-h-20 border-gray-200 focus-within:text-green-100">
             {legenda  &&
-                <legend className="uppercase text-xxs text-inherit mb-2">{legenda}</legend>
+                <legend className="uppercase text-xxs text-inherit mb-2 w-">{legenda}</legend>
             }
 
             <input className= {classMerge(["border-gray-300 mb-2 rounded-lg border w-full h-12 px-4 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300", variants.input[variant]])} type={type} {...rest} />
