36700f7004fb3e173ff0b037fc14566b1e0f3eb1
Philipi Pastor <philipipastor123@gmail.com>
2026-08-17 22:37:02 -0300

    style: ajustar responsividade do AuthLayout

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -4,8 +4,8 @@ import  logo  from "../assets/logo.svg"
 
 export function AuthLayout(){
     return(
-        <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center text-gray-100">
-            <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:min-2-[462px]">
+        <div className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center text-gray-100 p-8">
+            <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:max-2-[462px]">
                 <img src={logo} alt="logo" className="my-8"/>
                 <Outlet/>
             </main>
