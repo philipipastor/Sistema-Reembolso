57762b6dc3ff44ccb96d327a6d6e34cb322298b7
Philipi Pastor <philipipastor123@gmail.com>
2026-08-20 22:53:45 -0300

    feat: integrar API ao Dashboard de reembolsos

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { AxiosError } from "axios"
+
+import { api } from "../services/api.ts"
+import { type RefundsPaginationApiResponse } from "../dtos/refunds.ts"
 
 import { Input } from "../components/input.tsx"
 import { Button } from "../components/Button.tsx"
@@ -10,24 +14,53 @@ import search from "../assets/search.svg"
 import { categories } from "../utils/categoria.ts"
 import { formatCurrency } from "../utils/formatCurrency.ts"
 
-const exemplo = {
-    id: "123", 
-    nome: "ph",
-    category: "transporte",
-    amount: formatCurrency(34.50),
-    categoryIcon: categories["transport"].icon
-}
+const perPage =  5
 
 export function Dashboard(){
 
     const [name, setName] = useState("")
     const [page, setPage] = useState(1)
-    const [totalPage, setTotalPage] = useState(10)
-    const [refunds, setRefunds] = useState<PropsRefundItems[]>([exemplo])
+    const [totalPage, setTotalPage] = useState(0)
+    const [refunds, setRefunds] = useState<PropsRefundItems[]>([])
 
-    function fetchRefunds(e: React.FormEvent){
-        e.preventDefault()
+    async function fetchRefunds(){
+
+        try {
+            const response = await api.get<RefundsPaginationApiResponse>(`/refunds?name=${name.trim()}&page=${page}&perPage=${perPage}`)    
+            console.log(response.data)
+            
+        setRefunds(
+            response.data.refunds.map((refund) => ({
+                id: refund.id,
+                name: refund.user.name,
+                description: refund.name,
+                amount: formatCurrency(refund.amount),
+                categoryIcon: categories[refund.category].icon,
+            }))
+            )
+
+        setTotalPage(response.data.pagination.totalPages)
+                
+            
+        } catch (error) {
+            console.log(error)
+
+            if(error instanceof AxiosError){
+                alert(error.response?.data.message)
+            }
+
+            alert("Não foi possível carregar")
+        }
     }
+
+    function onSubmit(e:React.FormEvent) {
+        e.preventDefault()
+        fetchRefunds()
+    }
+
+    useEffect(() => {
+        fetchRefunds()
+    },[page])
 
     function handlePagination(action: "next" | "back") {
         setPage((prevPage) => {
@@ -47,7 +80,7 @@ export function Dashboard(){
         <div className="bg-gray-500 rounded-xl p-10 md:min-win-[768px]">
             <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
-            <form className="flex flex-1 items-center justify-between pb-6 border-b border-b-gray-400 mmd:flex-row gap-2 mt-6" onSubmit={fetchRefunds}>
+            <form className="flex flex-1 items-center justify-between pb-6 border-b border-b-gray-400 mmd:flex-row gap-2 mt-6" onSubmit={onSubmit}>
                 <Input placeholder="Pesquisar pelo nome" variant="manager" onChange={(e) => setName(e.target.value)}/>
                 <Button type="submit" variant="icon">
                     <img src={search} alt="ícone de pesquisa" className="w-5"/>
@@ -56,7 +89,7 @@ export function Dashboard(){
 
             <div className="my-6 flex flex-col gap-4 max-h-85.5 overflow-y-scroll">
                 {refunds.map((item) => 
-                <RefundItems key={item.id} data={exemplo} href={`/refund/${item.id}`}/>
+                <RefundItems key={item.id} data={item} href={`/refund/${item.id}`}/>
                 )}
             </div>
             
